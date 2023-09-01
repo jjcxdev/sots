1ca1f2257d5a0dc34643522f986042d242f38cd9
jjcxdev <justinjchambers@gmail.com>
2023-09-01 15:20:50 -0400

    & format issue

--- src/app/components/info.tsx
+++ src/app/components/info.tsx
@@ -4,10 +4,10 @@ export default function Info() {
       <div className="py-12 px-4 bg-white text-neutral-600">
         We are pleased to announce that the 1st annual Seminar on the Seas
         Custom Integration Training Course will take place in April of 2025
-        aboard Royal Caribbean&&apos;s Symphony of The Seas®.
+        aboard Royal Caribbean&apos;s Symphony of The Seas®.
         <br />
         <br />
-        It&&apos;s one of the world&&apos;s largest mega-cruise ships in the
+        It&apos;s one of the world&apos;s largest mega-cruise ships in the
         Oasis® class of cruise ships.This amazing 7-night Eastern Caribbean
         cruise will depart Miami, Florida on Sunday, April 13th and cruise to
         CocoCay, Bahamas; Charlotte Amalie, St. Thomas; and Philipsburg, St.
@@ -18,7 +18,7 @@ export default function Info() {
         Cruising on the Symphony of the Seas® boasts multiple distinct
         neighbourhoods, each offering a wide array of activities, amenities and
         entertainment. For the action-seeking cruiser, the ship offers two surf
-        simulators, two 43&&apos;-high rock-climbing walls, 3 water slides, an
+        simulators, two 43&apos;-high rock-climbing walls, 3 water slides, an
         escape room challenge, Laser Tag, and mini golf. For a more relaxed
         time, visit the full-service Vitality Spa, Fitness Center, or soak up
         some sun at one of the four swimming pools.
