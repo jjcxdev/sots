f0b40b8667eaa92ab21e271d0f9a6d57e0276d19
jjcx <112969641+jjcxdev@users.noreply.github.com>
2023-09-04 16:41:15 -0400

    course text size

--- src/app/components/faqcard.tsx
+++ src/app/components/faqcard.tsx
@@ -7,10 +7,10 @@ type CardProps = {
 function Card({ title, children }: CardProps) {
   return (
     <div className="w-full m-auto max-w-4xl flex rounded-md flex-col justify-center">
-      <div className="bg-custom-blue p-4 uppercase font-extrabold text-xl border border-custom-blue">
+      <div className="bg-custom-blue p-4 uppercase font-extrabold lg:text-3xl text-xl border border-custom-blue">
         {title}
       </div>
-      <div className="border p-4 text-neutral-600 text-base border-custom-blue">
+      <div className="border p-4 lg:text-2xl text-neutral-600 text-base border-custom-blue">
         {children}
       </div>
     </div>
